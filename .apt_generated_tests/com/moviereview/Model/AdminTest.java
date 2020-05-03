1e5993cfc6e1114e844364c0cf57cdc091d34dd1
NobleKatana <61984832+NobleKatana@users.noreply.github.com>
2020-05-03 18:42:44 -0500

    added firsts tests
    tests for admin create

---
+++ .apt_generated_tests/com/moviereview/Model/AdminTest.java
@@ -0,0 +1,43 @@
+package com.moviereview.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.runner.RunWith;
+
+class AdminTest {
+
+	@RunWith(JUnitPlatform.class)
+	@TestInstance(Lifecycle.PER_CLASS)
+	class AccountTestGet {
+		
+		Admin admin;
+		
+		@BeforeAll
+		public void setup() {
+			 admin = new Admin(0, "admin", "password");
+		}
+		
+		@Test
+		public void testGetUsername(){
+			Assertions.assertEquals("admin", admin.getUsername());
+		}
+		
+		@Test
+		public void testGetPassword(){
+			Assertions.assertEquals("password", admin.getPassword());
+		}
+		
+		@Test
+		public void testGetID(){
+			Assertions.assertEquals(0, admin.getId());
+		}
+		
+	
+
+	}
+}
---
+++ .apt_generated_tests/com/moviereview/Model/ReviewTest.java
@@ -0,0 +1,14 @@
+package com.moviereview.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReviewTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
