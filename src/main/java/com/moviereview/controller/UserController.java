075d2963f810f2947c68d7976f397ceaca61c66e
Jack Li <38816464+lijaack@users.noreply.github.com>
2020-05-07 22:20:03 -0700

    Update UserController.java

--- src/main/java/com/moviereview/controller/UserController.java
+++ src/main/java/com/moviereview/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -90,4 +91,10 @@ public class UserController {
 			List<User> users =this.userService.getAllUsers();
 			return users;
 	}
+	
+	@GetMapping(value = "/ec2Test")
+	public String sayHelloWorld() {
+	 return "Hello from ec2";
+	}
+
 }
