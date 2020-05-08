18a5f4ef25c0ac132de68ffb0a1f88df8b3e4816
Jack Li <38816464+lijaack@users.noreply.github.com>
2020-05-08 00:12:28 -0700

    BAD PRACTICE

--- src/main/java/com/moviereview/util/HibernateConfiguration.java
+++ src/main/java/com/moviereview/util/HibernateConfiguration.java
@@ -11,7 +11,13 @@ public class HibernateConfiguration {
 	public static Session getSession() {
 		try {
 			if (sessionFactory == null) {
-				sessionFactory = new Configuration().configure("/hibernate.cfg.xml").buildSessionFactory();
+//				sessionFactory = new Configuration().configure("/hibernate.cfg.xml").buildSessionFactory();
+				sessionFactory = new Configuration().configure()						
+						.setProperty("hibernate.connection.url", "jdbc:postgresql://movie-db.crr8wy1b38vr.us-west-1.rds.amazonaws.com/moviereview")
+						.setProperty("hibernate.connection.username", "postgres")
+						.setProperty("hibernate.connection.password", "password").buildSessionFactory();
+
+
 			}
 			return sessionFactory.getCurrentSession();
 		} catch (HibernateException e) {
